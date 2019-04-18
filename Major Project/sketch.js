9dc703fea7c1d717e44015d0bf7ffecbe3b8eef8
warrenbodes <47368856+warrenbodes@users.noreply.github.com>
2019-04-18 15:15:03 -0600

    Added the Template file

---
+++ Major Project/sketch.js
@@ -0,0 +1,17 @@
+// Major Project
+// Pouya Pourhaj && Jienan Chen
+// June 20, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+ll
